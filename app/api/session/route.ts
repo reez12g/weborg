acfa1f1b14b1ffb8a98860383bbcab0820a56364
reez12g <reez12g@gmail.com>
2023-08-12 11:06:59 +0900

    add session protection

--- app/api/session/route.ts
+++ app/api/session/route.ts
@@ -5,6 +5,13 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   const session = await getServerSession(authOptions);
 
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ status: "fail", message: "You are not signed in" }),
+      { status: 401 }
+    );
+  }
+
   return NextResponse.json({
     authenticated: !!session,
     session,
