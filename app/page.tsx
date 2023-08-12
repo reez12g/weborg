aaef651c96f8813f0e65ddf2fd318c37fc5de1e0
reez12g <reez12g@gmail.com>
2023-08-12 12:42:10 +0900

    add sidebar to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { SignInButton } from "./components/buttons.component";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 
@@ -9,9 +8,6 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <SignInButton />
-        <h1>Server Session</h1>
-        <pre>{JSON.stringify(session)}</pre>
       </div>
     </main>
   )
